26f808b63408571957008a60c3ecbfd33e9942bf
Michael Timbrook <timbrook480@gmail.com>
2017-01-30 14:19:59 -0500

    removed auto fetch, changed edge config

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,17 +1,10 @@
 import 'whatwg-fetch';
-import { loadTasks } from './tasks';
+// import { loadTasks } from './tasks';
 // import { requestFreshAuthToken } from './auth';
 
 export function init() {
-    return (dispatch: any) => {
+    return (_dispatch: any) => {
         // Auth check, TODO, use refresh
         // dispatch(requestFreshAuthToken());
-
-        // Auto refresh
-        setInterval(() => {
-            if (!document.hidden) {
-                dispatch(loadTasks());
-            }
-        }, 60000);
     };
 };
