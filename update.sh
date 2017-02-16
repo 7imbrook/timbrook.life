6aa1d984779691b86ac4f4a6bbb980a765178b7d
Michael Timbrook <timbrook480@gmail.com>
2017-02-16 11:22:56 -0500

    Removed aws rds stuff, didn't work

--- update.sh
+++ update.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+set -e
 
-docker service update --secret-add rds_host life_api
-docker service update --secret-add jwtkey life_api
-docker service update --secret-add aws_rds_life_password life_api
+# Force update stack, requires secret reattachment
+docker stack deploy -c docker-compose.yml life
+
+# Needed secrets, waiting for compose to get secrets
+for service in life_auth life_postgres life_api; do
+    docker service update --secret-add postgres_password $service;
+done
+
+for service in life_auth life_api; do
+    docker service update --secret-add jwtkey $service;
+done
