baac557f05172998f8c8840a9d43627b9428565c
Michael Timbrook <timbrook480@gmail.com>
2017-02-17 20:13:35 -0500

    Upgraded to compse 3.1 format w/ secrets

--- update.sh
+++ update.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 set -e
 
-# Force update stack, requires secret reattachment
+# Makes it only a little easier
 docker stack deploy -c docker-compose.yml life
-
-# Needed secrets, waiting for compose to get secrets
-for service in life_auth life_postgres life_api; do
-    docker service update --secret-add postgres_password $service;
-done
-
-for service in life_auth life_api; do
-    docker service update --secret-add jwtkey $service;
-done
-
-# postgres needs to start with the password attached or the persisted data
-# will not let the password get set
-# TODO: handle password changes
-docker service scale life_postgres=1
