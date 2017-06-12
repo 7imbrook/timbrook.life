0b6eb563685776d003a114bb06ba20e7f2df8a31
Michael Timbrook <timbrook480@gmail.com>
2017-06-12 18:51:20 -0400

    Added swarm manager state to volume to recover from node failures

--- infra/timbrook.life.tf
+++ infra/timbrook.life.tf
@@ -1,16 +1,14 @@
+# Access to digital ocean, docker cloud password can be
+# your generated api token (and should be)
 variable "do_token" {}
 variable "docker_cloud_password" {}
 
+# Set up the digital ocean provider
 provider "digitalocean" {
   token = "${var.do_token}"
 }
 
-# NOTE: just add this out of ban, then add the private key to ssh-agent
-# resource "digitalocean_ssh_key" "default" {
-#   name       = "Discovered SSH key"
-#   public_key = "${file("~/.ssh/id_rsa.pub")}"
-# }
-
+# This is the main web server, We init a swarm master and start the min required services
 resource "digitalocean_droplet" "web" {
   image  = "docker-16-04"
   name   = "timbrook.life"
@@ -19,7 +17,9 @@ resource "digitalocean_droplet" "web" {
   ssh_keys = [
     "71:00:ac:ea:c9:12:e5:af:cf:a6:af:15:8e:72:72:9c"
   ]
+  volume_ids = ["${digitalocean_volume.swarm-backup.id}"]
 
+  # Rexray volume management, used for databases inside of docker
   provisioner "file" {
     destination = "/tmp/config.yml"
     content = <<EOF
@@ -35,24 +35,35 @@ libstorage:
 EOF
   }
 
+  # maybe make this chef, or copy in a script
   provisioner "remote-exec" {
     inline = [
       "apt-get update",
       "apt-get upgrade -y",
+      "curl -sSL https://agent.digitalocean.com/install.sh | sh",
       "docker info",
       "mkdir -p /etc/rexray/",
       "cp /tmp/config.yml /etc/rexray/rexray.yml",
       "curl -sSL https://dl.bintray.com/emccode/rexray/install | sh -",
       "systemctl start rexray",
-      "echo public ipv4 ${digitalocean_droplet.web.ipv4_address}",
-      "docker swarm init --advertise-addr ${digitalocean_droplet.web.ipv4_address}",
-      # TODO: Remove old swarm from docker cloud
-      "docker run -ti --rm -v /var/run/docker.sock:/var/run/docker.sock dockercloud/registration --user 7imbrook --swarmname 7imbrook/life --pass ${var.docker_cloud_password}"
+      "mount -o discard,defaults /dev/disk/by-id/scsi-0DO_Volume_swarmbackup /var/lib/docker/swarm;",
+      " echo /dev/disk/by-id/scsi-0DO_Volume_swarmbackup /var/lib/docker/swarm ext4 defaults,nofail,discard 0 0 | sudo tee -a /etc/fstab",
+      # Because we're mounting in the raft backup, that means that all the services *should* come back online
+      "docker swarm init --force-new-cluster --advertise-addr ${digitalocean_droplet.web.ipv4_address}",
     ]
   }
 
 }
 
+# Attach the swarm manager raft log to backup recovery
+resource "digitalocean_volume" "swarm-backup" {
+  region      = "nyc1"
+  name        = "swarmbackup"
+  size        = 10
+  description = "Swarm raft backup"
+}
+
+# External IP
 resource "digitalocean_floating_ip" "external_ip" {
   droplet_id = "${digitalocean_droplet.web.id}"
   region     = "${digitalocean_droplet.web.region}"
