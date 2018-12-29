ca23dc645cf782bf4af7388018dbf7466bf4972d
Michael Timbrook <timbrook@fb.com>
2018-12-29 14:50:25 -0800

    Most host our

--- auto-mailer/mailgun.py
+++ auto-mailer/mailgun.py
@@ -4,6 +4,8 @@ from requests.auth import HTTPBasicAuth
 
 
 BASE_API_URL = "https://api.mailgun.net/v3"
+DOMAIN = os.environ.get("MAILGUN_DOMAIN", "timbrook.tech")
+HOST = os.environ.get("BASE_URL", "https://timbrook.tech")
 
 auth = HTTPBasicAuth("api", os.environ.get("MAILGUN_KEY"))
 
@@ -14,12 +16,12 @@ class Mailer:
 
     def send_resume_link(self, token):
         return requests.post(
-            f"{BASE_API_URL}/timbrook.tech/messages",
+            f"{BASE_API_URL}/{DOMAIN}/messages",
             auth=auth,
             data={
-                "from": "Robo Bob <no-reply@timbrook.tech>",
+                "from": f"Michael Timbrook <no-reply@{DOMAIN}>",
                 "to": [self.email],
-                "subject": "Hello",
-                "html": f"Here's your access link <a href='https://staging.timbrook.tech/resume?t={token}'>Here</a>. It will remain active for 3 days.",
+                "subject": "Resume Access",
+                "html": f"Here's your access link <a href='{HOST}/resume?t={token}'>Here</a>. It will remain active for 3 days.",
             },
         )
