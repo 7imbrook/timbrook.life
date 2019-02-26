53d57f27825205a67d6fae7e1c8fb67fcdc1ef31
Michael Timbrook <timbrook480@gmail.com>
2019-02-25 21:55:47 -0800

    Thats better

---
+++ landing/webpack.common.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractCssChunks = require("extract-css-chunks-webpack-plugin")
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/index.jsx',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          ExtractCssChunks.loader,
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+        ],
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.css'],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  plugins: [
+    new CleanWebpackPlugin(['./dist']),
+    new ExtractCssChunks(
+      {
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: "[hash]-[name].css",
+        chunkFilename: "[id].[hash].css",
+        hot: true, // if you want HMR - we try to automatically inject hot reloading but if it's not working, add it to the config
+        orderWarning: true, // Disable to remove warnings about conflicting order between imports
+        reloadAll: true, // when desperation kicks in - this is a brute force HMR flag
+        cssModules: true // if you use cssModules, this can help.
+      }
+    ),
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+};
---
+++ landing/webpack.prod.js
@@ -0,0 +1,32 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const S3Uploader = require('webpack-s3-uploader')
+const WebpackShellPlugin = require('webpack-shell-plugin');
+
+
+module.exports = merge(common, {
+  mode: 'production',
+  output: {
+    filename: '[hash].bundle.js',
+    publicPath: 'https://timbrook.sfo2.cdn.digitaloceanspaces.com/' 
+  },
+  plugins: [
+    new S3Uploader({
+      exclude: ['index.html'],
+      s3Options: {
+        accessKeyId: process.env.ACCESS_KEY_ID,
+        secretAccessKey: process.env.SECRET_ACCESS_KEY,
+        endpoint: 'sfo2.digitaloceanspaces.com'
+      },
+      s3UploadOptions: {
+        Bucket: 'timbrook'
+      },
+    }),
+    new WebpackShellPlugin({
+      onBuildEnd: [
+        'pwd',
+        'cp ./dist/index.html ./conf/',
+      ]
+    }),
+  ]
+});
