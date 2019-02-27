ee60fb96c5d0b5af0fbff5f3a581e5d6730972eb
Michael Timbrook <timbrook480@gmail.com>
2019-02-27 11:09:24 -0800

    Added entrypoints for 404

--- landing/src/index.jsx
+++ landing/src/index.jsx
@@ -1,43 +1,27 @@
 import './css/main.scss';
-import headshot from './images/headshot.jpg';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import QuoteWiget from './components/QuoteWiget';
-import QuoteProvider from './components/QuoteProvider';
+import Home from './entrypoints/Home';
+import NotFound from './entrypoints/404';
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+  } from 'react-router-dom'
 
 const root = document.getElementById('root');
 
 ReactDOM.render(
     (
-        <div className="site">
-            <header className="header">
-                <img src={headshot} alt="Author Image" className="headshot" />
-                <h1 className="title">Michael Timbrook</h1>
-                <h4 className="description">Software Engineer</h4>
-            </header>
-
-            <section className="quote">
-                <QuoteProvider>
-                    <QuoteWiget quote={'They had a look of terror on their face when I told them I found another distributed message broker.'}/>
-                </QuoteProvider>
-            </section>
-
-            <section className="social">
-                <a href="https://twitter.com/7imbrook"><i className="fab fa-twitter" data-full-name="Twitter" aria-hidden="true"></i></a>
-                <a href="https://www.instagram.com/7imbrook"><i className="fab fa-instagram" data-full-name="Instagram" aria-hidden="true"></i></a>
-                <a href="https://github.com/7imbrook"><i className="fab fa-github" data-full-name="GitHub" aria-hidden="true"></i></a>
-                <a href="https://linkedin.com/in/michaeltimbrook"><i className="fab fa-linkedin" data-full-name="Linkedin" aria-hidden="true"></i></a>
-                <a href="mailto:contact@timbrook.tech"><i className="far fa-envelope" data-full-name="Email" aria-hidden="true"></i></a>
-                {/* Will comeback to this, not looking for job right now */}
-                {/* <a href="/resume"><i className="fas fa-file-alt" data-full-name="Resume"></i></a> */}
-            </section>
-
-            <footer className="footer">
-                <h6 className="copyright">© 2018. Michael Timbrook. <a href="http://creativecommons.org/licenses/by/3.0/">Some Rights Reserved</a>.</h6>
-            </footer>
-        </div>
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route component={NotFound} />
+            </Switch>
+        </Router>
     ),
     root,
 );
