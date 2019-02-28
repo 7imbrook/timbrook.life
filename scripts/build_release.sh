baa543becdd57af68f99b31f9f5affb04f05ee53
Michael Timbrook <timbrook480@gmail.com>
2019-02-27 22:00:19 -0800

    No changes should get deployed

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -4,8 +4,19 @@ IMAGE=$1
 CONTEXT=$2
 LOG_KEY=$3
 
+ACTIVE_SHA=$(git log --format=format:%H -n 1)
+DIRLAST_SHA=$(git log --format=format:%H -n 1 .)
+
 # Make in root
 mkdir -p ~/logs
+
+if [ "$ACTIVE_SHA" != "$DIRLAST_SHA" ]
+then
+    echo "No changes in directory, skipping build"
+    touch ~/logs/${LOG_KEY}_push.log 
+    exit 0
+fi;
+
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker build -t $IMAGE $CONTEXT
 docker push $IMAGE | tee ~/logs/${LOG_KEY}_push.log
--- scripts/deploy_helm_app.sh
+++ scripts/deploy_helm_app.sh
@@ -3,4 +3,9 @@
 RELEASE_NAME=$1;
 APP_SHA=$2;
 
+if [[ -z "${APP_SHA}" ]]; then
+    echo "No new sha skip deploy"
+    exit 0;
+fi
+
 helm upgrade -i -f ./values.yaml --set image.sha=$APP_SHA $RELEASE_NAME personal/appshell;
