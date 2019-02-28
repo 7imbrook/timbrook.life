74b2a7be51e2a3ad93762fafebd5def6fc0d5364
Michael Timbrook <timbrook480@gmail.com>
2019-02-28 11:32:54 -0800

    Use hash in prod (useful)

--- landing/webpack.prod.js
+++ landing/webpack.prod.js
@@ -8,6 +8,7 @@ module.exports = merge(common, {
   mode: 'production',
   output: {
     filename: '[hash].bundle.js',
+    chunkFilename: '[hash].chunk.bundle.js',
     publicPath: 'https://timbrook.sfo2.cdn.digitaloceanspaces.com/' 
   },
   plugins: [
