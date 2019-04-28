6e40e865fab5e58dbfa86f38ff57c75f83f83505
Michael Timbrook <timbrook480@gmail.com>
2019-04-27 17:33:48 -0700

    Only use default quote

--- frontend/src/components/QuoteProvider.jsx
+++ frontend/src/components/QuoteProvider.jsx
@@ -2,13 +2,7 @@ import React from 'react';
 
 class QuoteProvider extends React.Component {
     componentWillMount() {
-        this.preload = window.preload !== undefined ? window.preload : [
-            {
-                id: 1,
-                quote: "They had a look of terror on their face when I told them I found another distributed message broker.",
-            },
-        ];
-        this.quote = this.preload[0].quote;
+        this.quote = "They had a look of terror on their face when I told them I found another distributed message broker."
     }
 
     render() {
