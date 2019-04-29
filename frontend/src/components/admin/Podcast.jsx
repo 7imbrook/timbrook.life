4dbe6cd2bc3be47dff657e7b6c72e73c1279b953
Michael Timbrook <timbrook480@gmail.com>
2019-04-28 20:22:37 -0700

    Tables :)

--- frontend/src/components/admin/Podcast.jsx
+++ frontend/src/components/admin/Podcast.jsx
@@ -5,7 +5,7 @@ import { loadFullPodcasts } from 'actions/podcasts';
 
 class Podcast extends Component {
 
-    componentWillMount(){
+    componentWillMount() {
         if (this.props.loadPodcast) {
             this.props.loadPodcast(this.props.podId);
         }
@@ -14,28 +14,36 @@ class Podcast extends Component {
     render() {
         const { podcast } = this.props;
         if (podcast === undefined) {
-            return <div>...</div>;
+            return <div><h3>Loading...</h3></div>;
         }
         const { name, description, episodes } = podcast;
         return (
             <div>
                 <h3>{name}</h3>
                 <sub>{description}</sub>
-                <hr/>
-                <code>
-                    {JSON.stringify(episodes)}
-                </code>
+                <hr />
+                <table>
+                    {episodes.map(ep =>
+                        <tr key={ep.id}>
+                            <td>
+                                <a href={ep.url}>{ep.name}</a>
+                            </td>
+                            <td>{ep.description}</td>
+                            <td>{ep.duration}</td>
+                        </tr>
+                    )}
+                </table>
             </div>
         );
     }
 }
 
-const mapStateToProps = (state , ownProps) => {
+const mapStateToProps = (state, ownProps) => {
     return {
         podcast: state.podcasts[ownProps.podId]
     }
 }
-  
+
 const mapDispatchToProps = {
     loadPodcast: loadFullPodcasts
 };
