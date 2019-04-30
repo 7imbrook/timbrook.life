992537e29ec0ef50fb69af119c23120dd5d0aa75
Michael Timbrook <timbrook480@gmail.com>
2019-04-30 08:26:32 -0700

    [skici] script change to deploy all

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -7,16 +7,32 @@ LOG_KEY=$3
 ACTIVE_SHA=$(git log --format=format:%H -n 1)
 DIRLAST_SHA=$(git log --format=format:%H -n 1 .)
 
+git show -q --pretty="%s" | grep "\[\(deployall\)\]" >> /dev/null
+DEPLOY_ALL=$?
+
 # Make in root
 mkdir -p ~/logs
+touch ~/logs/${LOG_KEY}_push.log 
 
-if [ "$ACTIVE_SHA" != "$DIRLAST_SHA" ]
+function build() {
+    docker login -u $DOCKER_USER -p $DOCKER_PASS
+    docker build -t $IMAGE $CONTEXT
+    docker push $IMAGE | tee ~/logs/${LOG_KEY}_push.log
+}
+
+if [ "$ACTIVE_SHA" == "$DIRLAST_SHA" ]
 then
-    echo "No changes in directory, skipping build"
-    touch ~/logs/${LOG_KEY}_push.log 
-    exit 0
+    build;
+    exit 0;
 fi;
 
-docker login -u $DOCKER_USER -p $DOCKER_PASS
-docker build -t $IMAGE $CONTEXT
-docker push $IMAGE | tee ~/logs/${LOG_KEY}_push.log
+if [ $DEPLOY_ALL -eq 0 ]
+then
+    echo "deploy all flag found"
+    build
+    exit 0;
+fi;
+
+# failed in some way?
+exit 1;
+
