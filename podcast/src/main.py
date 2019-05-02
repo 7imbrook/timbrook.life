1110d082bab8d9b8a7ab75405efa6872379a5419
Michael Timbrook <timbrook480@gmail.com>
2019-05-01 18:48:10 -0700

    Use new cdn and read from storage keys

--- podcast/src/main.py
+++ podcast/src/main.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 from flask_api import FlaskAPI
 from src.utils import require_valid_token
+from src.podcast import populate_episode, populate_podcast
 
 key = os.environ.get("ACCESS_KEY_ID")
 secret = os.environ.get("SECRET_ACCESS_KEY")
@@ -15,75 +16,6 @@ s3 = boto3.client(
     endpoint_url="https://sfo2.digitaloceanspaces.com",
 )
 app = FlaskAPI(__name__)
-
-
-def populate_episode(doc, data):
-    item = doc.new_tag("item")
-    bare_tags = {
-        "title": data["name"],
-        "itunes:duration": data["duration"],
-        "description": data["description"],
-        "itunes:subtitle": data["description"],
-        "itunes:summary": data["description"],
-    }
-    for t, v in bare_tags.items():
-        tag = doc.new_tag(t)
-        tag.string = v
-        item.append(tag)
-
-    guid = doc.new_tag("guid", isPermaLink="flase")
-    guid.string = data["url"]
-    item.append(guid)
-
-    item.append(doc.new_tag("enclosure", url=data["url"], type="audio/mpeg"))
-
-    return item
-
-
-def populate_podcast(doc, channel, podcast):
-    # basics
-    bare_tags = {
-        "title": podcast["name"],
-        "description": podcast["description"],
-        "language": "en-us",
-        "docs": "http://www.rssboard.org/rss-specification",
-        "generator": "myself",
-        "lastBuildDate": datetime.now().ctime(),
-    }
-    for t, v in bare_tags.items():
-        tag = doc.new_tag(t)
-        tag.string = v
-        channel.append(tag)
-
-    # Links
-    link = podcast["url"]
-    lt = doc.new_tag("link")
-    lt.string = link
-    channel.append(lt)
-
-    lta = doc.new_tag("atom:link", href=link, rel="self")
-    channel.append(lta)
-
-    # iTunes category and friends
-    cat = doc.new_tag("itunes:category", text="Technology")
-    cat.append(doc.new_tag("itunes:category", text="Podcasting"))
-    channel.append(cat)
-
-    channel.append(
-        doc.new_tag(
-            "itunes:image",
-            href="https://timbrook-podcast.sfo2.digitaloceanspaces.com/podcover.png",
-        )
-    )
-    expl = doc.new_tag("itunes:explicit")
-    expl.string = "yes"
-    channel.append(expl)
-
-    # Episodes
-    for ep in podcast["episodes"]:
-        channel.append(populate_episode(doc, ep))
-
-    return channel
 
 
 def get_podcasts(id):
