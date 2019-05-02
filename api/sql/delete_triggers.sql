c6fd11abe78394c419aa47e1ef93902bb563c161
Michael Timbrook <timbrook480@gmail.com>
2019-05-01 21:21:21 -0700

    Publish to rabbitmq when change on episodes

---
+++ api/sql/delete_triggers.sql
@@ -0,0 +1,30 @@
+CREATE OR REPLACE FUNCTION process_episode_change ()
+    RETURNS TRIGGER
+    AS $ep_trigger$
+BEGIN
+    IF (TG_OP = 'DELETE') THEN
+        PERFORM
+            pg_notify('purge_asset', row_to_json(OLD)::text);
+        RETURN OLD;
+    ELSIF (TG_OP = 'UPDATE') THEN
+        PERFORM
+            pg_notify('update_permission', row_to_json(NEW)::text);
+        RETURN NEW;
+    ELSIF (TG_OP = 'INSERT') THEN
+        PERFORM
+            pg_notify('update_permission', row_to_json(NEW)::text);
+        RETURN NEW;
+    END IF;
+    RETURN NULL;
+    -- result is ignored since this is an AFTER trigger
+END;
+$ep_trigger$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER episodes_cleanup
+    AFTER INSERT
+    OR UPDATE
+    OR DELETE ON episodes
+    FOR EACH ROW
+    EXECUTE PROCEDURE process_episode_change ();
+
